07f8bd66da3cbae87d53d47ba17279dc584edadd
Andrew Barcus <43078362+AndrewBarc@users.noreply.github.com>
2018-10-15 16:29:13 -0400

    Updated Budget.vb to write to excel files

--- Practice/Practice/Budget.vb
+++ Practice/Practice/Budget.vb
@@ -4,6 +4,13 @@
     End Sub
 
     Private Sub Excel_Click(sender As Object, e As EventArgs) Handles Excel.Click
-
+        Dim textFileStream As New IO.FileStream("C:\Windows\Temp\test.csv", IO.FileMode.OpenOrCreate, IO.FileAccess.ReadWrite, IO.FileShare.None)
+        Dim myFileWriter As New IO.StreamWriter(textFileStream)
+        Dim intCounter As Integer
+        For intCounter = 0 To 10
+            myFileWriter.WriteLine(CStr(intCounter) & ",")
+        Next intCounter
+        myFileWriter.Close()
+        textFileStream.Close()
     End Sub
 End Class
