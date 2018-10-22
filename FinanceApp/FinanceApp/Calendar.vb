0dc80ab94d2ec51f5a6896c20eb573bd04d1ab09
John Hall <jh791615@ohio.edu>
2018-10-22 15:55:18 -0400

    Added calendar form

--- FinanceApp/FinanceApp/Calendar.vb
+++ FinanceApp/FinanceApp/Calendar.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Calendar
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
 
+    End Sub
+
+    Private Sub CalendarDate_ValueChanged(sender As Object, e As EventArgs) Handles MyBaseDate.ValueChanged
+
+    End Sub
 End Class
