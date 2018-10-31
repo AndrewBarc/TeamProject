d38966c587938c93b3f563e0cf92485a1d0ce61c
Andrew Barcus <43078362+AndrewBarc@users.noreply.github.com>
2018-10-31 17:02:56 -0400

    Added new report design, doesn't have any functionality.

--- FinanceApp/FinanceApp/Reports/ReportMenu.vb
+++ FinanceApp/FinanceApp/Reports/ReportMenu.vb
@@ -1,13 +1,20 @@
 ﻿Public Class ReportMenu
+    Dim year As String
+    Dim month As String
+    Dim day As String
+
     Private Sub YearButton_Click(sender As Object, e As EventArgs) Handles YearButton.Click
         YearReport.Show()
+        year = YearReport.get_year()
+
+
+    End Sub
+    ' write more functions and less whatever subs are
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        ListBox1.Items.Add(year)
     End Sub
 
     Private Sub MonthButton_Click(sender As Object, e As EventArgs) Handles MonthButton.Click
-        MonthReport.Show()
-    End Sub
-
-    Private Sub SpecificButton_Click(sender As Object, e As EventArgs) Handles SpecificButton.Click
-        SpecificReport.Show()
+        MsgBox(year)
     End Sub
 End Class
