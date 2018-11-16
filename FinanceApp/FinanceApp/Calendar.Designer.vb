d905db4c3f0d19ed07f9d7a49f6f6fa11cdfb0a6
WyattNSmith <ws998116@ohio.edu>
2018-11-15 19:09:17 -0500

    Moved code from Calender.Designer.vb to Calendar.vb

--- FinanceApp/FinanceApp/Calendar.Designer.vb
+++ FinanceApp/FinanceApp/Calendar.Designer.vb
@@ -244,146 +244,14 @@ Partial Class Calendar
 
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
-
-    Dim TotalAmount As Double = 0
-
-
-    Private Sub Calendar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        AddColumnsToGridView()
-        Dim filePath As String
-        Dim line As String
-        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
-        Dim in_stream As New System.IO.StreamReader(filePath)
-        Dim currentDate As String = DateTime.Now.ToString("MM/dd/yyyy")
-
-        Do While in_stream.Peek() <> -1
-            line = in_stream.ReadLine() & vbNewLine
-            in_Date = line.Substring(0, 10)
-            If in_Date = currentDate Then
-                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
-                in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
-                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
-                AddRowToGridView()
-            End If
-        Loop
-        ' DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
-        in_stream.Close()
-    End Sub
-
-    Sub UpdateTotal(i As Double)
-        TotalAmount = TotalAmount + i
-    End Sub
-
-#Region "Data Table"
-
-    Dim dt As New DataTable
-    Dim in_Date As String
-    Dim in_Description As String
-    Dim in_Category As String
-    Dim in_Amount As String
-
-    Sub AddColumnsToGridView()
-
-        dt.Columns.Add("Date")
-        dt.Columns.Add("Description")
-        dt.Columns.Add("Category")
-        dt.Columns.Add("Amount")
-
-    End Sub
-
-    Sub AddRowToGridView()
-
-        Dim R As DataRow = dt.NewRow
-
-        R("Date") = in_Date
-        R("Description") = in_Description
-        R("Category") = in_Category
-        R("Amount") = in_Amount
-
-        dt.Rows.Add(R)
-
-        DataGridView1.DataSource = dt
-        UpdateTotal(in_Amount)
-        TextBox1.Text = TotalAmount.ToString()
-
-    End Sub
-
-#End Region
-
-    Public Function GetNthIndex(s As String, t As Char, n As Integer) As Integer
-        Dim count As Integer = 0
-        For i As Integer = 0 To s.Length - 1
-            If s(i) = t Then
-                count += 1
-                If count = n Then
-                    Return i
-                End If
-            End If
-        Next
-        Return -1
-    End Function
-
     Friend WithEvents TableLayoutPanel4 As TableLayoutPanel
     Friend WithEvents DataGridView1 As DataGridView
-
-
     Friend WithEvents MaskedTextBox1 As MaskedTextBox
-
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
-    End Sub
-
-    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles MaskedTextBox1.MaskInputRejected
-
-    End Sub
-
     Friend WithEvents TableLayoutPanel5 As TableLayoutPanel
     Friend WithEvents SearchButton As Button
-
-    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
-        dt.Clear()
-        Dim filePath As String
-        Dim line As String
-        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
-        Dim in_stream As New System.IO.StreamReader(filePath)
-        Do While in_stream.Peek() <> -1
-            line = in_stream.ReadLine() & vbNewLine
-            in_Date = line.Substring(0, 10)
-            If in_Date = MaskedTextBox1.Text Then
-                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
-                in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
-                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
-                AddRowToGridView()
-            End If
-        Loop
-        DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
-        in_stream.Close()
-    End Sub
-
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Button1 As Button
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        DataGridView1.Rows.Clear()
-        Dim filePath As String
-        Dim line As String
-        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
-        Dim in_stream As New System.IO.StreamReader(filePath)
-        Do While in_stream.Peek() <> -1
-            line = in_stream.ReadLine() & vbNewLine
-            in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
-            If in_Category = ComboBox1.Text Then
-                in_Date = line.Substring(0, 10)
-                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
-                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
-                AddRowToGridView()
-            End If
-        Loop
-        DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
-        in_stream.Close()
-    End Sub
-
     Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
     Friend WithEvents MonthCalendar1 As MonthCalendar
     Friend WithEvents Label1 As Label
--- FinanceApp/FinanceApp/Calendar.vb
+++ FinanceApp/FinanceApp/Calendar.vb
@@ -1,1 +1,129 @@
-﻿
+﻿Imports System.IO
+Public Class Calendar
+
+#Region "Form Events"
+
+    Dim TotalAmount As Double = 0
+
+    Private Sub Calendar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddColumnsToGridView()
+        Dim filePath As String
+        Dim line As String
+        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
+        Dim in_stream As New System.IO.StreamReader(filePath)
+        Dim currentDate As String = DateTime.Now.ToString("MM/dd/yyyy")
+
+        Do While in_stream.Peek() <> -1
+            line = in_stream.ReadLine() & vbNewLine
+            in_Date = line.Substring(0, 10)
+            If in_Date = currentDate Then
+                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
+                in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
+                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
+                AddRowToGridView()
+            End If
+        Loop
+        ' DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
+        in_stream.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        dt.Rows.Clear()
+        Dim filePath As String
+        Dim line As String
+        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
+        Dim in_stream As New System.IO.StreamReader(filePath)
+        Do While in_stream.Peek() <> -1
+            line = in_stream.ReadLine() & vbNewLine
+            in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
+            If in_Category = ComboBox1.Text Then
+                in_Date = line.Substring(0, 10)
+                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
+                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
+                AddRowToGridView()
+            End If
+        Loop
+        DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
+        in_stream.Close()
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        dt.Clear()
+        Dim filePath As String
+        Dim line As String
+        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, "data.txt")
+        Dim in_stream As New System.IO.StreamReader(filePath)
+        Do While in_stream.Peek() <> -1
+            line = in_stream.ReadLine() & vbNewLine
+            in_Date = line.Substring(0, 10)
+            If in_Date = MaskedTextBox1.Text Then
+                in_Description = line.Substring(GetNthIndex(line, ",", 1) + 1, (GetNthIndex(line, ",", 2) - GetNthIndex(line, ",", 1)) - 1)
+                in_Category = line.Substring(GetNthIndex(line, ",", 2) + 1, GetNthIndex(line, ",", 3) - (GetNthIndex(line, ",", 2)) - 1)
+                in_Amount = line.Substring(GetNthIndex(line, ",", 3) + 1, GetNthIndex(line, ",", 4) - (GetNthIndex(line, ",", 3)) - 1)
+                AddRowToGridView()
+            End If
+        Loop
+        DataGridView1.Sort(DataGridView1.Columns("Date"), System.ComponentModel.ListSortDirection.Descending)
+        in_stream.Close()
+    End Sub
+
+#End Region
+
+#Region "Data Table"
+
+    Dim dt As New DataTable
+    Dim in_Date As String
+    Dim in_Description As String
+    Dim in_Category As String
+    Dim in_Amount As String
+
+    Sub AddColumnsToGridView()
+
+        dt.Columns.Add("Date")
+        dt.Columns.Add("Description")
+        dt.Columns.Add("Category")
+        dt.Columns.Add("Amount")
+
+    End Sub
+
+    Sub AddRowToGridView()
+
+        Dim R As DataRow = dt.NewRow
+
+        R("Date") = in_Date
+        R("Description") = in_Description
+        R("Category") = in_Category
+        R("Amount") = in_Amount
+
+        dt.Rows.Add(R)
+
+        DataGridView1.DataSource = dt
+        UpdateTotal(in_Amount)
+        TextBox1.Text = TotalAmount.ToString()
+
+    End Sub
+
+#End Region
+
+#Region "Functions"
+
+    Public Function GetNthIndex(s As String, t As Char, n As Integer) As Integer
+        Dim count As Integer = 0
+        For i As Integer = 0 To s.Length - 1
+            If s(i) = t Then
+                count += 1
+                If count = n Then
+                    Return i
+                End If
+            End If
+        Next
+        Return -1
+    End Function
+
+    Sub UpdateTotal(i As Double)
+        TotalAmount = TotalAmount + i
+    End Sub
+
+#End Region
+
+End Class
